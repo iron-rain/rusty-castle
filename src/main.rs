2a2472e9370676db5e8cc0f0405c9d164b081c0d
Andrew Young <adyoung@ironrain.io>
2022-02-05 09:54:20 +1100

    monster movement done, page 183, taking break to run through rustlings

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,9 @@ fn main() -> BError {
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule
 }
 
 impl State {
@@ -66,6 +68,7 @@ impl State {
             .skip(1)
             .map(|r| r.center())
             .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         resources.insert(TurnState::AwaitingInput);
@@ -73,7 +76,9 @@ impl State {
         State {
             ecs,
             resources,
-            systems: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler()
         }
     }
 }
@@ -85,7 +90,22 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+
+        match current_state {
+            TurnState::AwaitingInput => self.input_systems.execute(
+                &mut self.ecs,
+                &mut self.resources
+            ),
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs, &mut self.resources);
+            },
+            TurnState::MonsterTurn => {
+                self.monster_systems.execute(&mut self.ecs, &mut self.resources);
+            }
+        }
+
         render_draw_buffer(ctx).expect("Render error");
     }
 }

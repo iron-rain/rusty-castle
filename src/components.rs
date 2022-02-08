847c2aa510a75cf79a3963161d60e9e4397fd9f3
Andrew Young <adyoung@ironrain.io>
2022-02-09 07:28:21 +1100

    movement on ecs now, page 189

--- src/components.rs
+++ src/components.rs
@@ -16,3 +16,9 @@ pub struct Enemy;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantToMove {
+    pub entity: Entity,
+    pub destination: Point
+}
